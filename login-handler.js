2775aecd6db933c3c6964e8eb1053c6534a56314
rc.github.io <rachelcheniphone@gmail.com>
2026-04-10 23:17:27 -0500

    Create login handler for GitHub OAuth

---
+++ login-handler.js
@@ -0,0 +1,23 @@
+// GitHub OAuth login handler
+const express = require('express');
+const passport = require('passport');
+const GitHubStrategy = require('passport-github2').Strategy;
+
+passport.use(new GitHubStrategy({
+    clientID: 'YOUR_GITHUB_CLIENT_ID',
+    clientSecret: 'YOUR_GITHUB_CLIENT_SECRET',
+    callbackURL: '/auth/github/callback'
+}, (accessToken, refreshToken, profile, done) => {
+    return done(null, profile);
+}));
+
+const app = express();
+
+app.get('/auth/github', passport.authenticate('github', { scope: [ 'user:email' ] }));
+
+app.get('/auth/github/callback', passport.authenticate('github', { failureRedirect: '/' }), (req, res) => {
+    res.redirect('/');
+});
+
+// Export the app
+module.exports = app;
